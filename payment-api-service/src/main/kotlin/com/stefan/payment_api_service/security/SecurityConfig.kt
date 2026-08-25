5a56b6efa2fd50cdfd54c64f353fe9c680da035f
a2966607 <a2966607@adelaide.edu.au>
2026-08-25 16:01:16 +0930

    style(security): add missing space before function brace

--- payment-api-service/src/main/kotlin/com/stefan/payment_api_service/security/SecurityConfig.kt
+++ payment-api-service/src/main/kotlin/com/stefan/payment_api_service/security/SecurityConfig.kt
@@ -9,7 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder
 class SecurityConfig {
 
     @Bean
-    fun passwordEncoder(): PasswordEncoder{
+    fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
     }
 }
